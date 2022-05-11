4b14f827d9b59598938647ecc856831940ac8b44
passosleo <leonardopassos98@gmail.com>
2022-05-11 10:51:35 -0300

    feat: layout

--- App.js
+++ App.js
@@ -5,61 +5,22 @@ import {
   TouchableOpacity,
   StyleSheet,
   Image,
+  TextInput,
 } from 'react-native';
 import logo from './src/assets/images/react-keycloak-logo.png';
-import Login from 'react-native-login';
-
-const config = {
-  url: 'http://20.206.80.63:8585/',
-  realm: 'lighthouse',
-  client_id: 'react-native-app',
-  redirect_uri: 'keycloak://app',
-  appsite_uri: 'keycloak://app',
-  kc_idp_hint: 'facebook',
-};
 
 const App = () => {
-  const [tokens, setTokens] = useState({});
-
-  useEffect(() => {
-    Login.tokens()
-      .then(tokens => {
-        setTokens(tokens);
-      })
-      .catch(() => {
-        setTokens(null);
-      });
-  }, []);
-
-  const onLogin = () => {
-    Login.start(config)
-      .then(tokens => {
-        setTokens(tokens);
-      })
-      .catch(() => {
-        setTokens(null);
-      });
-  };
-
-  const onLogout = () => {
-    Login.end();
-    setTokens(null);
-  };
-
-  // const details = Login.decodeToken(tokens.access_token);
-
-  console.log(tokens);
-  // console.log(details);
+  const [accessToken, setAccessToken] = useState("");
+  const [refreshToken, setRefresToken] = useState("");
 
   return (
     <SafeAreaView style={styles.container}>
-      <Image source={logo} />
-      <Text style={styles.title}>Keycloak React Native Demo App</Text>
-      <TouchableOpacity onPress={() => onLogin()} style={styles.button}>
+      <Image style={styles.logo} source={logo} />
+      <Text style={styles.title}>Keycloak React Native Custom Login</Text>
+      <TextInput style={styles.input} placeholder="User"/>
+      <TextInput style={styles.input} placeholder="Password"/>
+      <TouchableOpacity style={styles.button}>
         <Text style={styles.text}>Login</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => onLogout()} style={styles.button}>
-        <Text style={styles.text}>Logout</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
@@ -72,10 +33,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    width: 120,
+    height: 50,
+  },
   title: {
     fontSize: 22,
-    color: '#000',
-    marginVertical: 64,
+    color: '#505050',
+    marginVertical: 30,
+    maxWidth: 250,
+    textAlign: 'center',
+  },
+  input: {
+    width: 300,
+    fontSize: 18,
+    color: '#505050',
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    marginBottom: 30,
   },
   button: {
     backgroundColor: '#00a5e5',
